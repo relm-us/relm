d12d58c643bc19afd00f390457edb09a2dbd2f93
Duane Johnson <duane.johnson@gmail.com>
2020-10-29 07:32:35 -0600

    Title and wording

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	let name: string = 'world';
+  let name: string = "world";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
 
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
 
-		@media (min-width: 640px) {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Pieces of Relm are coming together.</p>
+</main>
