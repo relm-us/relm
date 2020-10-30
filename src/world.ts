4ef728a2fc1bf33cc1883aa8540f192618353082
Duane Johnson <duane.johnson@gmail.com>
2020-10-29 18:01:59 -0600

    Composite benchmark of 4096 dancing chairs

--- src/world.ts
+++ src/world.ts
@@ -17,6 +17,7 @@ import { OscillateSystem } from "./systems/OscillateSystem";
 import { CompositeTransformSystem } from "./systems/CompositeTransformSystem";
 import { CenteredMeshSystem } from "./systems/CenteredMeshSystem";
 import { Euler } from "three";
+import Stats from "stats.js";
 
 const world = new World({
   plugins: [ThreePlugin],
@@ -50,6 +51,35 @@ function makeBox({ x = 0, y = 0, z = 0, w = 1, h = 1, d = 1, color = "red" }) {
     });
 }
 
+function makeChair({ x = 0, y = 0, z = 0 }) {
+  return world.entities
+    .create("Chair")
+    .add(CenteredMeshSystem)
+    .add(CompositeTransform, {
+      position: new Vector3(x, y, z),
+      scale: new Vector3(0.1, 0.1, 0.1),
+      offsets: {
+        static: {
+          position: new Vector3(0, -0.15, 0),
+        },
+      },
+    })
+    .add(Model, {
+      asset: new Asset("/chair.glb"),
+    })
+    .add(OscillateRotation, {
+      phase: Math.random() * 100,
+      min: new Quaternion().setFromEuler(new Euler(0, 0, -Math.PI / 4)),
+      max: new Quaternion().setFromEuler(new Euler(0, 0, Math.PI / 4)),
+    })
+    .add(OscillatePosition, {
+      frequency: 2,
+      max: new Vector3(0, 0.5, 0),
+    })
+    .add(Noisy, { speed: 2, magnitude: new Vector3(0, 0, 10) })
+    .activate();
+}
+
 // Create the floor
 const origin = makeBox({
   y: -0.505,
@@ -59,46 +89,17 @@ const origin = makeBox({
   color: "white",
 }).activate();
 
-const chair = world.entities
-  .create("Chair")
-  .add(CenteredMeshSystem)
-  .add(CompositeTransform, {
-    position: new Vector3(0, 0, 0),
-    scale: new Vector3(1, 1, 1),
-    offsets: {
-      static: {
-        position: new Vector3(0, -0.15, 0),
-      },
-    },
-  })
-  .add(Model, {
-    asset: new Asset("/chair.glb"),
-  })
-  .add(OscillateRotation, {
-    min: new Quaternion().setFromEuler(new Euler(0, 0, -Math.PI / 4)),
-    max: new Quaternion().setFromEuler(new Euler(0, 0, Math.PI / 4)),
-  })
-  .add(OscillatePosition, {
-    frequency: 2,
-    max: new Vector3(0, 0.5, 0),
-  })
-  .add(Noisy, { speed: 2, magnitude: new Vector3(0, 0, 10) })
-  .activate();
-
-const orangeBox = makeBox({ x: 0, y: 0, z: -2, color: "orange" })
-  .add(Noisy, { speed: 4, magnitude: new Vector3(0, 2, 4) })
-  .activate();
-
-const blueBox = makeBox({ x: -2, y: 0, z: 0, color: "blue" })
-  .add(Noisy, { speed: 4, magnitude: new Vector3(0, 2, 4) })
-  .activate();
-
-const brownBox = makeBox({ x: 2, y: 0, z: 0, color: "brown" })
-  .add(Noisy, { speed: 4, magnitude: new Vector3(0, 2, 4) })
-  .activate();
+// ~100 ms per frame
+for (let dep = -8; dep < 8; dep++) {
+  for (let row = 0; row < 16; row++) {
+    for (let col = -8; col < 8; col++) {
+      makeChair({ x: col / 4, y: row / 4, z: dep / 4 });
+    }
+  }
+}
 
 // Create the singleton camera
-const cameraBoom = new Vector3(0, 5, 5);
+const cameraBoom = new Vector3(0, 8, 6);
 const camera = world.entities
   .create("Camera")
   .add(CompositeTransform, {
@@ -115,16 +116,22 @@ const camera = world.entities
   .activate();
 
 (window as any).world = world;
-(window as any).chair = chair;
+// (window as any).chair = chair;
 
 import("@dimforge/rapier3d").then((RAPIER) => {
   const gravity = new RAPIER.Vector3(0.0, -9.81, 0.0);
   const physWorld = new RAPIER.World(gravity);
 });
 
+const stats = new Stats();
+stats.showPanel(1);
+document.body.appendChild(stats.dom);
+
 let previousTime = 0;
 let mag = 0;
 const gameLoop = (time) => {
+  stats.update();
+
   const delta = time - previousTime;
   world.update(delta);
   previousTime = time;
