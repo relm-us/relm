5c9d092416106276fb52385fdaf678477ab811bc
Duane Johnson <duane.johnson@gmail.com>
2020-11-07 11:59:40 -0700

    Add stats.js

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import App from "./App.svelte";
-// import "./world";
+import Stats from "stats.js";
+
+const stats = new Stats(1);
+document.body.append(stats.dom);
+
+function updateStats() {
+  stats.update();
+  requestAnimationFrame(updateStats);
+}
+updateStats();
 
 const app = new App({
   target: document.body,
