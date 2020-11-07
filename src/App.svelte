fb2c568564f4bd59b8d799f45e6690adf5dba5e3
Duane Johnson <duane.johnson@gmail.com>
2020-11-07 15:54:08 -0700

    Move stats.js to its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import RequiresRapier from "./RequiresRapier.svelte";
   import RenderedWorld from "./RenderedWorld.svelte";
+  import FrameRateStats from "./FrameRateStats.svelte";
 </script>
 
 <RequiresRapier>
   <RenderedWorld />
 </RequiresRapier>
+
+<FrameRateStats />
---
+++ src/FrameRateStats.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Stats from "stats.js";
+
+  const stats = new Stats(1);
+  document.body.append(stats.dom);
+
+  function updateStats() {
+    stats.update();
+    requestAnimationFrame(updateStats);
+  }
+  updateStats();
+</script>
