a72db1ec9732fe2f63c9075ff6bfe9a7e978cc34
Duane Johnson <duane.johnson@gmail.com>
2020-11-11 16:15:21 -0700

    Add WASD keys to ArrowKeyListener

--- src/input/ArrowKeyListener/ArrowKeyListener.svelte
+++ src/input/ArrowKeyListener/ArrowKeyListener.svelte
@@ -1,33 +1,35 @@
 <script lang="ts">
   import { keyUp, keyDown, keyLeft, keyRight } from "../store";
 
-  function setKeyStore(key, value) {
-    switch (key) {
-      case "ArrowUp":
-        keyUp.set(value);
-        return true;
-      case "ArrowDown":
-        keyDown.set(value);
-        return true;
-      case "ArrowLeft":
-        keyLeft.set(value);
-        return true;
-      case "ArrowRight":
-        keyRight.set(value);
-        return true;
+  const KEY_W = 87;
+  const KEY_A = 65;
+  const KEY_S = 83;
+  const KEY_D = 68;
+
+  function setKeyStore(key, keyCode, value) {
+    if (key === "ArrowUp" || keyCode === KEY_W) {
+      keyUp.set(value);
+    } else if (key === "ArrowDown" || keyCode === KEY_S) {
+      keyDown.set(value);
+    } else if (key === "ArrowLeft" || keyCode === KEY_A) {
+      keyLeft.set(value);
+    } else if (key === "ArrowRight" || keyCode === KEY_D) {
+      keyRight.set(value);
+    } else {
+      return false;
     }
-    return false;
+    return true;
   }
 
   function onKeydown(event) {
     if (event.repeat) return;
-    if (setKeyStore(event.key, true)) {
+    if (setKeyStore(event.key, event.keyCode, true)) {
       event.preventDefault();
     }
   }
 
   function onKeyup(event) {
-    setKeyStore(event.key, false);
+    setKeyStore(event.key, event.keyCode, false);
   }
 </script>
 
