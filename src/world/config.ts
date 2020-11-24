7acb3c70adc9de36d99739f1b8ae2aec88f21208
Duane Johnson <duane.johnson@gmail.com>
2020-11-24 16:40:28 -0700

    Add server config per environment

--- src/world/config.ts
+++ src/world/config.ts
@@ -2,5 +2,24 @@ export type ShadowMapConfig = "BASIC" | "PCF" | "VSM";
 export const shadowMapConfig: ShadowMapConfig = "VSM";
 
 export const connection = {
-  url: "ws://localhost:1234",
+  url: config(window.location).serverYjsUrl,
 };
+
+function config(location) {
+  let serverUrl: string;
+  let serverYjsUrl: string;
+  let serverUploadUrl: string;
+  if (location.origin === "https://relm.us") {
+    serverUrl = "https://y.relm.us";
+    serverYjsUrl = "wss://y.relm.us";
+  } else if (location.origin === "https://staging.relm.us") {
+    serverUrl = "https://y-staging.relm.us";
+    serverYjsUrl = "wss://y-staging.relm.us";
+  } else {
+    serverUrl = `http://${location.hostname}:1234`;
+    serverYjsUrl = `ws://${location.hostname}:1234`;
+  }
+  serverUploadUrl = `${serverUrl}/asset`;
+
+  return { serverUrl, serverYjsUrl, serverUploadUrl };
+}
