17f2ab4c9de37723185fc28f6074e1165484f521
Duane Johnson <duane.johnson@gmail.com>
2020-11-25 12:54:51 -0700

    Prevent accidentally selecting text on number drag

--- src/ui/EditorPanel/NumberDragger.ts
+++ src/ui/EditorPanel/NumberDragger.ts
@@ -24,6 +24,7 @@ export class NumberDragger {
     this.mousedown = ((event) => {
       this.mouseGrab = true;
       this.mouseStartValue = this.getValue();
+      event.preventDefault();
     }).bind(this);
 
     this.mouseup = ((event) => {
