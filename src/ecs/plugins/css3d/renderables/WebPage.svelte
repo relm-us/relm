f448ae4b6701a4237c665fd7f17d1e653ad42edc
Duane Johnson <duane.johnson@gmail.com>
2020-11-28 09:12:08 -0700

    Show red box around WebPage when has focus

--- src/ecs/plugins/css3d/renderables/WebPage.svelte
+++ src/ecs/plugins/css3d/renderables/WebPage.svelte
@@ -6,16 +6,46 @@
   export let url: string;
 
   let iframe;
+  let frameMouseOver = false;
+  let loaded = false;
+  let highlighted = false;
+
+  function onWindowBlur() {
+    if (frameMouseOver) {
+      highlighted = true;
+    }
+  }
+
+  function onWindowFocus() {
+    highlighted = false;
+  }
+
+  const onFrameLoad = (event) => {
+    loaded = true;
+  };
+
+  function onFrameMouseover() {
+    frameMouseOver = true;
+  }
+
+  function onFrameMouseout() {
+    frameMouseOver = false;
+  }
 </script>
 
 <style>
   iframe {
+    display: none;
+
     position: absolute;
     top: 0;
     left: 0;
     z-index: 0;
 
     pointer-events: auto;
+  }
+  iframe.show {
+    display: block;
   }
   overlay {
     position: absolute;
@@ -34,10 +64,21 @@
     background-color: rgb(240, 240, 240, 0.7);
     box-shadow: inset 0px 0px 0px 6px #000000;
   }
+  overlay.highlighted {
+    box-shadow: inset 0px 0px 0px 6px #ff4400;
+    background-color: rgb(0, 0, 0, 0);
+    pointer-events: none;
+  }
 </style>
+
+<svelte:window on:blur={onWindowBlur} on:focus={onWindowFocus} />
 
 <iframe
   bind:this={iframe}
+  on:load={onFrameLoad}
+  on:mouseover={onFrameMouseover}
+  on:mouseout={onFrameMouseout}
+  class:show={loaded}
   title="Web Page"
   {width}
   {height}
@@ -45,6 +86,6 @@
   frameborder="0"
   allowfullscreen />
 
-{#if $mode === 'build'}
-  <overlay />
+{#if $mode === 'build' || highlighted}
+  <overlay class:highlighted />
 {/if}
