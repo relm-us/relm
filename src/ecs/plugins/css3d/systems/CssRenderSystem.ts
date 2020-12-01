07e42da9b3c4ac3f180c5a3541573d77243a082c
Duane Johnson <duane.johnson@gmail.com>
2020-12-01 13:52:57 -0700

    Possible future CSS optimization for slow CPUs

--- src/ecs/plugins/css3d/systems/CssRenderSystem.ts
+++ src/ecs/plugins/css3d/systems/CssRenderSystem.ts
@@ -16,12 +16,17 @@ export class CssRenderSystem extends System {
     }
     this.presentation = presentation;
     this.cssPresentation = cssPresentation;
+
+    this.frame = 0;
   }
 
   update() {
     if (!this.cssPresentation.viewport || !this.presentation.viewport) return;
 
+    // Possible optimization for slow CPUs: skip every second frame
+    // if (this.frame++ % 2 === 0) {
     this.cssPresentation.updateCamera();
     this.cssPresentation.render();
+    // }
   }
 }
