8fe7daf1f14999b158fbf2bc918f3a3899a8b779
Duane Johnson <duane.johnson@gmail.com>
2021-01-24 11:40:37 -0700

    Use new USER_DROPPED trigger on SortableList

--- packages/relm-client/src/ui/LibraryPanel/SortableList.svelte
+++ packages/relm-client/src/ui/LibraryPanel/SortableList.svelte
@@ -1,6 +1,10 @@
 <script>
   import { flip } from "svelte/animate";
-  import { dndzone, SHADOW_ITEM_MARKER_PROPERTY_NAME } from "svelte-dnd-action";
+  import {
+    dndzone,
+    TRIGGERS,
+    SHADOW_ITEM_MARKER_PROPERTY_NAME,
+  } from "svelte-dnd-action";
   import { config } from "~/stores/config";
 
   import { dropzones } from "./dropzones";
@@ -14,7 +18,38 @@
     return `${$config.serverUploadUrl}/${filename}`;
   }
 
-  function acceptItems({ detail }) {
+  function consider(event) {
+    if (event.detail.info.trigger === TRIGGERS.USER_DROPPED) {
+      const mouse = event.detail.info.pointerClientXY;
+      const els = Array.from(document.elementsFromPoint(mouse.x, mouse.y));
+
+      let dropzoneId;
+      for (const el of els) {
+        if (el.dataset.dropzoneId) {
+          dropzoneId = el.dataset.dropzoneId;
+        }
+      }
+
+      if (dropzoneId) {
+        const item = event.detail.items.find(
+          (i) => i.id === event.detail.info.id
+        );
+        delete item[SHADOW_ITEM_MARKER_PROPERTY_NAME];
+        const dz = dropzones.get(dropzoneId);
+        // don't allow dropping on "self"
+        if (dz.list.id !== list.id) {
+          dz.dispatch("drop", { item, list });
+          dz.active.set(true);
+          // no animation
+          event.preventDefault();
+        }
+      }
+    } else {
+      items = event.detail.items;
+    }
+  }
+
+  function finalize({ detail }) {
     items = detail.items;
   }
 
@@ -40,6 +75,31 @@
     }
   }
 </script>
+
+<section
+  use:dndzone={{
+    items,
+    flipDurationMs: FLIP_DURATION,
+    dropTargetStyle: "",
+  }}
+  on:consider={consider}
+  on:finalize={finalize}
+>
+  {#each items as item (item.id)}
+    <item data-id={item.id} animate:flip={{ duration: FLIP_DURATION }}>
+      {#if item.thumbnail}
+        <img src={imageUrl(item.thumbnail)} alt="thumbnail" />
+      {:else}{item.name}<br />{item.id}{/if}
+      {#if item[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
+        <item class="custom-shadow-item">
+          {#if item.thumbnail}
+            <img src={imageUrl(item.thumbnail)} alt="thumbnail" />
+          {:else}{item.name}<br />{item.id}{/if}
+        </item>
+      {/if}
+    </item>
+  {/each}
+</section>
 
 <style>
   section {
@@ -87,23 +147,3 @@
     object-fit: contain;
   }
 </style>
-
-<section
-  use:dndzone={{ items, flipDurationMs: FLIP_DURATION, customDrop, dropTargetStyle: '' }}
-  on:consider={acceptItems}
-  on:finalize={acceptItems}>
-  {#each items as item (item.id)}
-    <item data-id={item.id} animate:flip={{ duration: FLIP_DURATION }}>
-      {#if item.thumbnail}
-        <img src={imageUrl(item.thumbnail)} alt="thumbnail" />
-      {:else}{item.name}<br />{item.id}{/if}
-      {#if item[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
-        <item class="custom-shadow-item">
-          {#if item.thumbnail}
-            <img src={imageUrl(item.thumbnail)} alt="thumbnail" />
-          {:else}{item.name}<br />{item.id}{/if}
-        </item>
-      {/if}
-    </item>
-  {/each}
-</section>
