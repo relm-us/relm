00b457880b84959726eb58c47726d61b90b915ee
Duane Johnson <duane.johnson@gmail.com>
2021-01-29 21:59:15 -0700

    Deselect any selected objects when entering 'play' mode

--- packages/relm-client/src/events/switchMode.ts
+++ packages/relm-client/src/events/switchMode.ts
@@ -1,9 +1,11 @@
 import { get } from "svelte/store";
 import { mode } from "~/stores/mode";
+import { selectedEntities } from "~/stores/selection";
 
 export function onSwitchMode() {
   const $mode = get(mode);
   if ($mode === "build") {
+    selectedEntities.clear();
     mode.set("play");
   } else if ($mode === "play") {
     mode.set("build");
