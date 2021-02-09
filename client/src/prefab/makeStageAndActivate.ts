84a1252ad67a19de55c7cacdaff7a9532079f9d1
Duane Johnson <duane.johnson@gmail.com>
2021-02-09 12:38:46 -0700

    Revert "Play with light and shadow"

--- client/src/prefab/makeStageAndActivate.ts
+++ client/src/prefab/makeStageAndActivate.ts
@@ -25,18 +25,21 @@ export function makeStageAndActivate(world, avatar) {
     .add(Camera)
     .activate();
 
-  const lightOffset = new Vector3(-10, 25, -10);
+  const lightOffset = new Vector3(-5, 5, 0);
   const lightPosition = new Vector3().add(cameraPosition).add(lightOffset);
-  const shadowSize = 5;
+  const shadowSize = 7.5;
   const light = makeEntity(world, "DirectionalLight")
     .add(Transform, {
       position: lightPosition,
     })
-    // .add(Follow, {
-    //   entity: camera.id,
-    //   limit: "XZ_AXIS",
-    //   offset: lightOffset,
+    // .add(LookAt, {
+    //   entity: avatar.id,
     // })
+    .add(Follow, {
+      entity: camera.id,
+      limit: "XYZ_AXIS",
+      offset: lightOffset,
+    })
     .add(DirectionalLight, {
       target: avatar.id,
       color: 0x999999,
@@ -46,8 +49,21 @@ export function makeStageAndActivate(world, avatar) {
       shadowBottom: -shadowSize,
       shadowFar: 100,
       shadowRadius: 1.75,
+      shadowDistanceGrowthRatio: 12,
     })
     .activate();
 
-  return { camera, light };
+  const sunLight = makeEntity(world, "DirectionalLight")
+    .add(Transform, {
+      position: new Vector3(5, 10, -20),
+    })
+    .add(Follow, {
+      entity: avatar.id,
+    })
+    // .add(DirectionalLight, {
+    //   color: 0xdddddd,
+    // })
+    .activate();
+
+  return { camera, light, sunLight };
 }
