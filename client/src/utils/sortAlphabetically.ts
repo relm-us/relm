244a265166188fc34c16cedeb56831a84e0875d0
Duane Johnson <duane.johnson@gmail.com>
2021-02-09 12:54:04 -0700

    Catch errors in sortAlphabetically

--- client/src/utils/sortAlphabetically.ts
+++ client/src/utils/sortAlphabetically.ts
@@ -1,7 +1,23 @@
 export function sortAlphabetically<T>(array: Array<T>, getter: (T) => string) {
   array.sort((a, b) => {
-    const A = getter(a).toUpperCase(); // ignore upper and lowercase
-    const B = getter(b).toUpperCase(); // ignore upper and lowercase
+    let A, B;
+
+    try {
+      A = getter(a);
+    } catch (err) {
+      console.trace(`sortAlphabetically getter failed`);
+    }
+    if (!A) A = "";
+    A = A.toUpperCase();
+
+    try {
+      B = getter(b);
+    } catch (err) {
+      console.trace(`sortAlphabetically getter failed`);
+    }
+    if (!B) B = "";
+    B = B.toUpperCase();
+
     if (A < B) return -1;
     if (A > B) return 1;
     return 0;
