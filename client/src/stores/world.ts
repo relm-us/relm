3fcb76da15cb8636c9c1d1df5810ccd2a377a601
Duane Johnson <duane.johnson@gmail.com>
2021-02-09 22:49:43 -0700

    Show error messages with traceback when world creation fails

--- client/src/stores/world.ts
+++ client/src/stores/world.ts
@@ -1,17 +1,37 @@
-import { readable, Readable } from "svelte/store";
+import { readable, derived, Readable } from "svelte/store";
 
 import { World } from "~/ecs/base";
 
 import { createWorld } from "../world/creation";
 
-export const world: Readable<World | null> = readable(null, (set) => {
+const rapier = readable(null, (set) => {
   import("@dimforge/rapier3d")
     .then((rapier) => {
-      const world = createWorld(rapier);
-
-      set(world);
+      set(rapier);
     })
     .catch((error) => {
       console.error("Can't load physics engine rapier3d", error.message);
     });
 });
+
+export const world: Readable<World> = derived(
+  rapier,
+  ($rapier, set) => {
+    if (!$rapier) return;
+    const world = createWorld($rapier);
+    set(world);
+  },
+  null
+);
+
+// export const world: Readable<World | null> = readable(null, (set) => {
+//   import("@dimforge/rapier3d")
+//     .then((rapier) => {
+//       const world = createWorld(rapier);
+
+//       set(world);
+//     })
+//     .catch((error) => {
+//       console.error("Can't load physics engine rapier3d", error.message);
+//     });
+// });
