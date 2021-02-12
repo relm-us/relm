f2c6a2c22b8b670220feba3c466d200e8c5ab7ad
Duane Johnson <duane.johnson@gmail.com>
2021-02-11 21:54:45 -0700

    Remove "Thing" prefab (doesn't do anything)

--- client/src/prefab/index.ts
+++ client/src/prefab/index.ts
@@ -37,7 +37,6 @@ export const directory = [
   { name: "Box", prefab: makeBox },
   { name: "Ball", prefab: makeBall },
   { name: "Image", prefab: makeImage },
-  { name: "Thing", prefab: makeThing },
   { name: "Label", prefab: makeLabel },
   { name: "Pile of Boxes", prefab: makePileOfBoxes },
   { name: "YouTube TV", prefab: makeTv },
