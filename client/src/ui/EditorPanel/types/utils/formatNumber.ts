fe4a5d1232eca9ccacda9225965a117083bbd3e0
Duane Johnson <duane.johnson@gmail.com>
2021-02-14 14:17:21 -0700

    Show high-precision numbers when editing value in Editor

--- client/src/ui/EditorPanel/types/utils/formatNumber.ts
+++ client/src/ui/EditorPanel/types/utils/formatNumber.ts
@@ -3,10 +3,10 @@ export function formatNumber(n, editing, digits = 1) {
     return n;
   } else if (typeof n === "number") {
     const fixed = n.toFixed(digits);
-    if (editing || !(editing && n.toString().length < fixed.length)) {
-      return fixed;
-    } else {
+    if (editing) {
       return n;
+    } else {
+      return fixed;
     }
   } else if (n === undefined) {
     return "[undefined]";
