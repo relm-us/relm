064bc16e86078edd86e1e323e3482acb655caf3f
Duane Johnson <duane.johnson@gmail.com>
2021-03-04 20:23:04 -0700

    Fix escape key only partially switched to play mode

--- client/src/events/escape.ts
+++ client/src/events/escape.ts
@@ -1,18 +1,18 @@
 import { get } from "svelte/store";
 import { Relm } from "~/stores/Relm";
 import { selectedEntities } from "~/stores/selection";
-import { chatOpen } from '~/stores/chatOpen'
-import { mode } from '~/stores/mode'
-
+import { chatOpen } from "~/stores/chatOpen";
+import { mode } from "~/stores/mode";
+import { onSwitchMode } from "./switchMode";
 
 export function onEscape() {
   const $Relm = get(Relm);
-  const selected = get(selectedEntities)
+  const selected = get(selectedEntities);
   if (selected.size > 0) {
-    $Relm.selection.clear()
+    $Relm.selection.clear();
   } else if (get(chatOpen)) {
-    chatOpen.set(false)
-  } else if (get(mode) !== 'play') {
-    mode.set('play')
+    chatOpen.set(false);
+  } else if (get(mode) !== "play") {
+    onSwitchMode("play");
   }
 }
