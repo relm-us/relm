917dfee83aa1879ddd171eeaf470fbd8fc2a2127
Duane Johnson <duane.johnson@gmail.com>
2021-04-25 20:12:54 -0600

    Fix occasional undefined target error on input event

--- client/src/input/isInputEvent.ts
+++ client/src/input/isInputEvent.ts
@@ -1,11 +1,13 @@
 export function isInputEvent(event) {
-  return (
-    // Regular text inputs and textareas should be able to use copy paste, arrow keys
-    event.target.tagName === "INPUT" ||
-    event.target.tagName === "TEXTAREA" ||
-    // We can make divs editable, and they should be able to copy/paste, etc.
-    event.target.getAttribute("contenteditable") !== null ||
-    // We can make divs receive focus, and they, too, should be able to copy/paste etc.
-    event.target.getAttribute("tabindex") !== null
-  );
+  if (!event.target) return false;
+  else
+    return (
+      // Regular text inputs and textareas should be able to use copy paste, arrow keys
+      event.target.tagName === "INPUT" ||
+      event.target.tagName === "TEXTAREA" ||
+      // We can make divs editable, and they should be able to copy/paste, etc.
+      event.target.getAttribute("contenteditable") !== null ||
+      // We can make divs receive focus, and they, too, should be able to copy/paste etc.
+      event.target.getAttribute("tabindex") !== null
+    );
 }
