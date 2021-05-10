0355afb1b80268c0fbcb55ea90e3f6fbcebbdc08
Duane Johnson <duane.johnson@gmail.com>
2021-05-09 20:16:13 -0600

    Add notes to TwistBone component

--- client/src/ecs/plugins/twist-bone/components/TwistBone.ts
+++ client/src/ecs/plugins/twist-bone/components/TwistBone.ts
@@ -1,13 +1,10 @@
-import {
-  LocalComponent,
-  BooleanType,
-  StringType,
-  NumberType,
-  RefType,
-} from "~/ecs/base";
+import { Vector3 } from "three";
+import { LocalComponent, StringType, NumberType, RefType } from "~/ecs/base";
 
 export class TwistBone extends LocalComponent {
   boneName: string;
+  function: Vector3;
+  speed: number;
 
   static props = {
     boneName: {
@@ -18,6 +15,14 @@ export class TwistBone extends LocalComponent {
       },
     },
 
+    /**
+     * A custom function that returns a target position (Vector3) that is in
+     * coordinates relative to the bone's position (i.e. bone is at 0,0,0).
+     * The bone will rotate (twist) towards that direction.
+     *
+     * NOTE: currently the twisting is axis-locked to Y.
+     * TODO: make axis lock configurable.
+     */
     function: {
       type: RefType,
       default: (entity) => {
@@ -34,14 +39,6 @@ export class TwistBone extends LocalComponent {
       default: 0.15,
       editor: {
         label: "Twist Speed",
-      },
-    },
-
-    enabled: {
-      type: BooleanType,
-      default: true,
-      editor: {
-        label: "Enabled?",
       },
     },
   };
