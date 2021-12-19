0dbbe711d65ca30046016deb98a00e7ef742a059
Duane Johnson <duane.johnson@gmail.com>
2021-12-19 09:28:12 -0700

    Show error detail when asset can't be loaded

--- client/src/ecs/plugins/asset/systems/AssetSystem.ts
+++ client/src/ecs/plugins/asset/systems/AssetSystem.ts
@@ -45,10 +45,12 @@ export class AssetSystem extends System {
 
     let url = entity.get(Asset)[type].url;
     if (!url) return this.loadingError(entity, `missing url ('${type}')`);
-    if (!url.startsWith("http") &&
-        // Some assets such as humanoid-003.glb are loaded from our local server's
-        // public folder, rather than the ourrelm asset server:
-        !url.startsWith("/")) {
+    if (
+      !url.startsWith("http") &&
+      // Some assets such as humanoid-003.glb are loaded from our local server's
+      // public folder, rather than the ourrelm asset server:
+      !url.startsWith("/")
+    ) {
       // relative URLs are assumed to be relative to our CDN asset server
       url = assetUrl(url);
     }
@@ -61,8 +63,14 @@ export class AssetSystem extends System {
       if (type === "texture") value = await this.presentation.loadTexture(url);
       else if (type === "model") value = await this.presentation.loadGltf(url);
     } catch (err) {
-      // TODO: when load fails with 404, say the asset can't be found rather than
-      // "SyntaxError: JSON.parse: unexpected character at line 1"
+      if (err.target) {
+        if (err.target instanceof HTMLImageElement) {
+          err = err.target.src;
+        } else if (err.target instanceof XMLHttpRequest) {
+          const t = err.target;
+          err = `${t.responseURL} ${t.status} ${t.statusText}`;
+        }
+      }
       return this.loadingError(entity, `unable to load asset: "${err}"`);
     }
 
