a98efb3885785589a033beac456af21bd81cdedc
Duane Johnson <duane.johnson@gmail.com>
2021-12-19 09:30:07 -0700

    Use GLTFLoader.load directly rather than node workaround

--- client/src/ecs/plugins/core/Loader.ts
+++ client/src/ecs/plugins/core/Loader.ts
@@ -1,44 +1,17 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-/**
- * GLTFLoader isn't quite supported in node.js so this
- * is a bit of hack to make it usable
- *
- * 1. load() tries to use XMLHttpRequest which doesn't exist
- *    on node. Instead we just load it ourself using fetch
- *    with is available in browsers, and polyfilled on node.
- *    It is then sent through parse() manually.
- * 2. parse() tries to call atob() on the result which node
- *    does not have. We polyfill this too.
- */
+const loader = new GLTFLoader();
 
 export class Loader {
   loader: GLTFLoader;
-  cache: object;
 
   constructor() {
     this.loader = new GLTFLoader();
-    this.cache = {};
   }
 
   async load(path) {
-    let promise = this.cache[path];
-    if (!promise) {
-      promise = this.cache[path] = new Promise(async (resolve, reject) => {
-        const response = await fetch(path);
-        const buffer = await response.arrayBuffer();
-        try {
-          this.loader.parse(
-            buffer,
-            null,
-            (gltf) => resolve(gltf),
-            (error) => reject(error)
-          );
-        } catch (err) {
-          reject(err);
-        }
-      });
-    }
-    return await promise;
+    return new Promise((resolve, reject) => {
+      loader.load(path, data=> resolve(data), null, reject);
+    });
   }
 }
