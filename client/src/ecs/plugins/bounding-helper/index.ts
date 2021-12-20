c85dc22b94ff4e0e6fe871ce33161dd7bddb0bdc
Duane Johnson <duane.johnson@gmail.com>
2021-12-20 16:20:17 -0700

    Add comment for bounding-helper

--- client/src/ecs/plugins/bounding-helper/index.ts
+++ client/src/ecs/plugins/bounding-helper/index.ts
@@ -8,6 +8,10 @@ export * from "./components";
 
 export { Components };
 
+/**
+ * Show a bounding box or bounding sphere around an object.
+ * Used mostly for build mode (currently when holding "shift" key).
+ */
 export default createPlugin({
   name: "bounding-helper",
   plugins: [CorePlugin],
