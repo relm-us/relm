3ef2cc2cb4c15ae47de07e4c35ea09f4f690d0ce
Duane Johnson <duane.johnson@gmail.com>
2021-12-20 22:39:36 -0700

    Simplify and fix Diamond prefab

--- client/src/prefab/makeDiamond.ts
+++ client/src/prefab/makeDiamond.ts
@@ -1,47 +1,19 @@
-import { Color, Vector3 } from "three";
+import { Vector3 } from "three";
 
 import { makeEntity } from "./makeEntity";
 
 import { Transform } from "~/ecs/plugins/core";
-import { Html2d } from "~/ecs/plugins/html2d";
 import { Diamond } from "~/ecs/plugins/diamond";
+import { Clickable } from "~/ecs/plugins/clickable";
 
-export function makeDiamond(
-  world,
-  { x = 0, y = 0, z = 0, yOffset = 1, content = "Hello" }
-) {
-  const linearColor = new Color("#BBFF00");
-  linearColor.convertSRGBToLinear();
-
+export function makeDiamond(world, { x = 0, y = 0, z = 0, yOffset = 1 }) {
+  // TODO: Why do we need to at 2 units to `y` value to get it above ground??
   const diamond = makeEntity(world, "Diamond")
     .add(Transform, {
-      position: new Vector3(x, y + yOffset, z),
+      position: new Vector3(x, y + yOffset + 2, z),
     })
-    .add(Html2d, {
-      kind: "LABEL",
-      content,
-      offset: new Vector3(0, -0.6, 0),
-      hanchor: 0,
-      vanchor: 0,
-      draggable: true,
-    })
+    .add(Clickable)
     .add(Diamond);
-
-  const message = makeEntity(world, "Message")
-    .add(Transform, {
-      position: new Vector3(),
-    })
-    .add(Html2d, {
-      kind: "LABEL",
-      content: "",
-      offset: new Vector3(0, 0.6, 0),
-      hanchor: 0,
-      vanchor: 0,
-      editable: true,
-      draggable: false,
-    });
-
-  message.setParent(diamond);
 
   return diamond;
 }
