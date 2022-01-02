001ccb2b400921fbe9f5192a71c95edfc5958eea
Duane Johnson <duane.johnson@gmail.com>
2022-01-01 22:36:02 -0700

    Only load physics engine once

--- client/src/main/effects/importPhysicsEngine.ts
+++ client/src/main/effects/importPhysicsEngine.ts
@@ -1,13 +1,16 @@
 import { Dispatch } from "../RelmStateAndMessage";
 
+let rapier;
+
 export async function importPhysicsEngine(dispatch: Dispatch) {
-  let rapier;
-  try {
-    rapier = await import("@dimforge/rapier3d");
-  } catch ({ errmsg, stack }) {
-    const message = `Can't load physics engine rapier3d: ${errmsg}`;
-    dispatch({ id: "error", message, stack });
-    return;
+  if (!rapier) {
+    try {
+      rapier = await import("@dimforge/rapier3d");
+    } catch ({ errmsg, stack }) {
+      const message = `Can't load physics engine rapier3d: ${errmsg}`;
+      dispatch({ id: "error", message, stack });
+      return;
+    }
   }
 
   dispatch({
