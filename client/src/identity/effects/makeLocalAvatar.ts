b4bfc910bbbce6b61edc196d8cf4d004daaeb0c8
Duane Johnson <duane.johnson@gmail.com>
2022-01-20 15:02:01 -0700

    Clean up makeLocalAvatar

--- client/src/identity/effects/makeLocalAvatar.ts
+++ client/src/identity/effects/makeLocalAvatar.ts
@@ -1,41 +1,16 @@
 import { Vector3 } from "three";
-import { get } from "svelte/store";
 import { DecoratedECSWorld } from "~/types/DecoratedECSWorld";
 
 import { Controller } from "~/ecs/plugins/player-control";
 import { TwistBone, headFollowsPointer } from "~/ecs/plugins/twist-bone";
 
 import { makeAvatarEntities } from "../Avatar/makeAvatarEntities";
-import { localIdentityData } from "../identityData";
-import { playerId } from "../playerId";
 import { Dispatch } from "../ProgramTypes";
-import { Appearance, Participant } from "../types";
 import { Avatar } from "../Avatar";
-import { setAvatarFromParticipant } from "../Avatar";
 
 export const makeLocalAvatar =
-  (
-    // localParticipant: Participant,
-    ecsWorld: DecoratedECSWorld,
-    position: Vector3
-    // clientId: number,
-    // appearance: Appearance
-  ) =>
-  (dispatch: Dispatch) => {
+  (ecsWorld: DecoratedECSWorld, position: Vector3) => (dispatch: Dispatch) => {
     const entities = makeAvatarEntities(ecsWorld, position, false);
-
-    // const identityData = get(localIdentityData);
-    // identityData.clientId = clientId;
-
-    // Participant may have chosen appearance from 'quick start' avatar screen
-    // if (appearance) identityData.appearance = appearance;
-
-    // const localParticipant: Participant = {
-    //   participantId: playerId,
-    //   isLocal: true,
-    //   modified: false,
-    //   identityData,
-    // };
 
     const avatar = new Avatar(ecsWorld, entities);
 
