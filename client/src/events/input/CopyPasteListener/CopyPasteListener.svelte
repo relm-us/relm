acb0fe0ca0848cad55cc69793259710531667117
Duane Johnson <duane.johnson@gmail.com>
2022-01-29 22:40:01 -0700

    Fix copy/paste in build mode inputs should behave as normal

--- client/src/events/input/CopyPasteListener/CopyPasteListener.svelte
+++ client/src/events/input/CopyPasteListener/CopyPasteListener.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { worldUIMode } from "~/stores/worldUIMode";
+  import { isInputEvent } from "../isInputEvent";
   import { copy } from "./copy";
   import { paste } from "./paste";
 
   function onCopy(event: ClipboardEvent) {
+    if (isInputEvent(event)) return;
     if ($worldUIMode === "build") {
       if (copy(event.clipboardData)) {
         event.preventDefault();
@@ -11,6 +13,7 @@
     }
   }
   function onPaste(event: ClipboardEvent) {
+    if (isInputEvent(event)) return;
     if ($worldUIMode === "build") {
       paste(event.clipboardData || (window as any).clipboardData);
     }
