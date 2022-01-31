2bbfd99a99a326d6ca8e6fa79580b4dc8f9b0db7
Duane Johnson <duane.johnson@gmail.com>
2022-01-31 10:07:39 -0700

    Add hemisphere light by default

--- client/src/world/createScene.ts
+++ client/src/world/createScene.ts
@@ -7,8 +7,8 @@ export function createScene() {
 
   scene.fog = new FogExp2(0xe5e0dd, 0.022);
 
-  // const hemiLight = new HemisphereLight(0xffffff, 0xffffff, 2);
-  // scene.add(hemiLight);
+  const hemiLight = new HemisphereLight(0xffffbb, 0x080820, 1);
+  scene.add(hemiLight);
 
   const ambientLight = new AmbientLight(0xffffff, 1);
   scene.add(ambientLight);
