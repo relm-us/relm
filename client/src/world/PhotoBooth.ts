c3ac6b353b20dd64c97a2f29275adcd21a54cbeb
Duane Johnson <duane.johnson@gmail.com>
2022-02-17 23:20:50 -0700

    Unwind object rotation for PhotoBooth

--- client/src/world/PhotoBooth.ts
+++ client/src/world/PhotoBooth.ts
@@ -7,6 +7,7 @@ import {
   Vector3,
   Vector2,
   Box3,
+  Sphere,
 } from "three";
 import { createScene } from "./createScene";
 
@@ -14,6 +15,7 @@ export class PhotoBooth {
   scene: Scene;
   container1: Object3D;
   container2: Object3D;
+  container3: Object3D;
 
   camera: PerspectiveCamera;
   renderer: WebGLRenderer;
@@ -34,9 +36,12 @@ export class PhotoBooth {
     scene.add(this.light);
 
     this.container1 = new Object3D();
-    scene.add(this.container1);
     this.container2 = new Object3D();
+    this.container3 = new Object3D();
+
+    scene.add(this.container1);
     this.container1.add(this.container2);
+    this.container2.add(this.container3);
 
     this.camera = new PerspectiveCamera(
       35,
@@ -50,29 +55,41 @@ export class PhotoBooth {
   }
 
   takeShot(object: Object3D) {
-    // Place object in center, and de-rotate
-    const parent = object.parent;
-    this.container2.add(object);
-    this.container2.position.copy(object.position).multiplyScalar(-1);
-    this.container1.quaternion.copy(object.quaternion).invert();
-
-    // Rotate a quarter-turn to the left, so objects aren't facing straight on to camera
-    this.container1.rotation.y -= Math.PI / 4;
-
     // Update camera aspect to current canvas width/height
     const canvasSize = new Vector2();
     this.renderer.getSize(canvasSize);
     this.camera.aspect = canvasSize.x / canvasSize.y;
     this.camera.updateProjectionMatrix();
 
-    // Move camera so that object takes up full view
+    // Save parent for restore step (last step)
+    const parent = object.parent;
+
+    // Add object to inner container, neutralizing position
+    this.container3.add(object);
+    this.container3.position.copy(object.position).multiplyScalar(-1);
+
+    // Neutralize rotation, making object face camera
+    this.container2.quaternion.copy(object.quaternion).invert();
+
+    // Measure object size
+    const box = new Box3().setFromObject(this.container1);
     const bbSize = new Vector3();
-    const box = new Box3().setFromObject(this.scene);
     box.getSize(bbSize);
+
+    // Place object in visual center
+    const center = new Vector3();
+    box.getCenter(center);
+    this.container1.position.copy(center).multiplyScalar(-1);
+
+    // Rotate a quarter-turn to the left, so objects aren't facing straight on to camera
+    this.container2.rotation.y -= Math.PI / 4;
+
+    // Move camera so that object takes up full view
     var fov = this.camera.fov * (Math.PI / 180);
     var distance = Math.abs(Math.min(bbSize.x, bbSize.y) / Math.sin(fov / 2));
     this.camera.position.set(0, 1, 1).normalize().multiplyScalar(distance);
-    this.camera.lookAt(this.container1.position);
+    // this.camera.lookAt(this.container1.position);
+    this.camera.lookAt(0, 0, 0);
 
     // Take photo
     this.renderer.render(this.scene, this.camera);
