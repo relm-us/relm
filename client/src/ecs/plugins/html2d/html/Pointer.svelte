04f8c65b65a6a827fe410fddfd3a4c83cc4c8b2d
Duane Johnson <duane.johnson@gmail.com>
2022-02-20 14:39:57 -0700

    Add sparse-octree dependency

---
+++ client/src/ecs/plugins/html2d/html/Pointer.svelte
@@ -0,0 +1,16 @@
+<script>
+  import BaseSvg from "~/ui/lib/BaseSvg.svelte";
+</script>
+
+<BaseSvg viewBox="0 0 1735 1735" {...$$props}>
+  <g transform="translate(-91.250857,-71.330667)">
+    <path
+      style="color:#000000;overflow:visible;fill:#ffffff;fill-opacity:1;stroke:#1a1a1a;stroke-width:1.87472;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stop-color:#000000"
+      d="m 93.647795,72.693662 a 3.0034202,3.0034202 0 0 0 -1.43328,2.973216 l 7.55291,56.537432 a 3.0034202,3.0034202 0 0 0 5.339295,1.45612 l 13.70827,-17.47294 21.98536,-3.13479 a 3.0034202,3.0034202 0 0 0 1.40861,-5.35202 L 97.023345,72.890499 a 3.0034202,3.0034202 0 0 0 -3.37555,-0.196837 z"
+    />
+    <path
+      d="m 105.76349,79.765517 c 0,0 -15.462546,-11.086785 -5.977684,5.281452 9.484864,16.368241 19.029184,31.140521 42.542324,27.532741 l -23.51314,3.60778 -13.69146,17.41482 c 0,0 -1.03994,1.7599 -3.43979,0.79995 -2.39986,-0.95994 -2.39986,-5.83965 -2.39986,-5.83965 L 92.444287,76.085736 c 0,0 -0.352819,-2.664393 1.203508,-3.392074 1.556327,-0.727681 2.916246,-0.327705 4.156173,0.712233 1.239926,1.039939 7.959522,6.359622 7.959522,6.359622 z"
+      style="fill:#000000;fill-opacity:0.0809717;fill-rule:evenodd;stroke:none;stroke-width:0.264583px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+    />
+  </g>
+</BaseSvg>
