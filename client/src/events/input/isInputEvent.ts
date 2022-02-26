e3f7fc5bda9910506bf8440f5b0e30c657dbc641
Duane Johnson <duane.johnson@gmail.com>
2022-02-26 14:40:39 -0700

    Fix 'copy' action in build mode

--- client/src/events/input/isInputEvent.ts
+++ client/src/events/input/isInputEvent.ts
@@ -5,16 +5,18 @@ export function isInputEvent(event) {
   // e.g. Upload dialog needs to be able to accept paste events
   if (get(uploadingDialogOpen)) return true;
 
-  if (!event.target || !event.target.getAttribute) return false;
-  else
+  if (!event.target || !event.target.getAttribute) {
+    return false;
+  } else {
     return (
       // Regular text inputs and textareas should be able to use copy paste, arrow keys
       event.target.tagName === "INPUT" ||
       event.target.tagName === "TEXTAREA" ||
       event.target.tagName === "IFRAME" ||
       // We can make divs editable, and they should be able to copy/paste, etc.
-      event.target.getAttribute("contenteditable") !== null ||
+      event.target.getAttribute("contenteditable") == "true" ||
       // We can make divs receive focus, and they, too, should be able to copy/paste etc.
       event.target.getAttribute("tabindex") !== null
     );
+  }
 }
