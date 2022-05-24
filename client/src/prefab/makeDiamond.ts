da77c780a0fc2146dd79d3a8ba644f251310d76b
Duane Johnson <duane.johnson@gmail.com>
2022-05-24 07:39:37 -0600

    Add Info panel to diamond prefab

--- client/src/prefab/makeDiamond.ts
+++ client/src/prefab/makeDiamond.ts
@@ -5,12 +5,19 @@ import { makeEntity } from "./makeEntity";
 import { Transform } from "~/ecs/plugins/core";
 import { Diamond } from "~/ecs/plugins/diamond";
 import { Clickable } from "~/ecs/plugins/clickable";
+import { Html2d } from "~/ecs/plugins/html2d";
 
 export function makeDiamond(world, { x = 0, y = 0, z = 0 }) {
-  const diamond = makeEntity(world, "Diamond")
+  return makeEntity(world, "Diamond")
     .add(Transform, { position: new Vector3(x, y + 0.5, z) })
+    .add(Diamond)
     .add(Clickable)
-    .add(Diamond);
-
-  return diamond;
+    .add(Html2d, {
+      kind: "INFO",
+      vanchor: 2,
+      editable: true,
+      visible: true,
+      zoomInvariant: false,
+      offset: new Vector3(0, 0.5, 0),
+    });
 }
