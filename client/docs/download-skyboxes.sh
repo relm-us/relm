89fff9dd77c843d4ad305f2d5220583ef3874248
Duane Johnson <duane.johnson@gmail.com>
2022-06-21 17:51:47 -0600

    Add instructions to download skyboxes for self-hosted assets

---
+++ client/docs/download-skyboxes.sh
@@ -0,0 +1,16 @@
+# This script will fetch the 8 images needed for the "default" skyboxes
+# in the Settings tab to work, if you are hosting assets locally.
+
+# Instructions:
+# 1. `cd` to your assets folder, e.g. `client/data/assets`
+# 2. make sure you have `wget` available
+# 3. run this script
+
+wget https://assets.ourrelm.com/edc3d0040ef1e1feece33adef09b32c4-7496.webp
+wget https://assets.ourrelm.com/5edd94b3d28b8a05c2858de0ae503e39-1586.webp
+wget https://assets.ourrelm.com/42e9b44e8e2cce697d446c85809a378c-79050.webp
+wget https://assets.ourrelm.com/fdeb9a7dab4c03b2befe637d55c8ea36-6908.webp
+wget https://assets.ourrelm.com/6c798e8ce73d162955743af2dd2c27ff-22658.webp
+wget https://assets.ourrelm.com/9f15e6be6c086594fbe8e389f4a209b1-4138.webp
+wget https://assets.ourrelm.com/b0c5c961730f92fbd97d3b8f4ad73f53-31160.webp
+wget https://assets.ourrelm.com/543ba08c91613b380a8c304bec4f11b5-4322.webp
