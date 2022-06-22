99c89d8815157e08bd2142254716b03088a9f93d
Duane Johnson <duane.johnson@gmail.com>
2022-06-21 18:29:04 -0600

    Add 36px, 40px font sizes to Document component

--- client/src/ecs/plugins/css3d/document/quill/quillInit.ts
+++ client/src/ecs/plugins/css3d/document/quill/quillInit.ts
@@ -7,7 +7,8 @@ import { worldManager } from "~/world";
 // prettier-ignore
 export const fontSizes = [
   "12px", "16px", "20px", "24px", "28px",
-  "32px", "48px", "80px", "100px", "150px"
+  "32px", "36px", "40px", "48px", "80px",
+  "100px", "150px"
 ];
 
 Quill.register("modules/cursors", QuillCursors);
