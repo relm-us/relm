6d291fbe2bba122f1caba1f5cf0ad93565296c20
Duane Johnson <duane.johnson@gmail.com>
2022-07-13 22:05:56 -0600

    WIP

--- client/src/ecs/plugins/css3d/document/quill/quillInit.ts
+++ client/src/ecs/plugins/css3d/document/quill/quillInit.ts
@@ -1,6 +1,8 @@
 import Quill from "quill";
 import QuillCursors from "quill-cursors";
 import { QuillBinding } from "y-quill";
+import ImageUploader from "quill-image-uploader";
+import { config } from "~/config";
 
 import { worldManager } from "~/world";
 
@@ -23,15 +25,44 @@ var Size = Quill.import("attributors/style/size");
 Size.whitelist = fontSizes;
 Quill.register(Size, true);
 
+let registeredImageUploader = false;
+
 export function quillInit(
   container,
   toolbar,
   { cursors = true, readOnly = false, bounds = container }
 ) {
+  if (toolbar && !registeredImageUploader) {
+    // Upload images rather than base64-encoding-and-embedding in Yjs
+    Quill.register("modules/imageUploader", ImageUploader);
+    registeredImageUploader = true;
+  }
+
   const editor = new Quill(container, {
     modules: {
       cursors,
       toolbar,
+      imageUploader: {
+        upload: (file) =>
+          new Promise((resolve, reject) => {
+            const formData = new FormData();
+            formData.append("file", file);
+
+            fetch(config.serverUploadUrl, {
+              method: "POST",
+              body: formData,
+            })
+              .then((response) => response.json())
+              .then((result) => {
+                console.log(result);
+                resolve(result.data.url);
+              })
+              .catch((error) => {
+                reject("Upload failed");
+                console.error("Error:", error);
+              });
+          }),
+      },
     },
     placeholder: "Start collaborating...",
     theme: "snow", // or 'bubble'
