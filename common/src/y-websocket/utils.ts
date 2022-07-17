3efd8d18c963aa5b2c00c1ae08c871ecc8bfd27c
WillQizza <williamqipizza@gmail.com>
2022-07-16 23:32:02 -0600

    add a warning message that appears when you do not set YPERSISTENCE

--- common/src/y-websocket/utils.ts
+++ common/src/y-websocket/utils.ts
@@ -45,6 +45,8 @@ if (typeof persistenceDir === "string") {
     },
     writeState: async (docName, ydoc) => {},
   };
+} else {
+  console.warn("Missing YPERSISTENCE environment variable. y-websocket data will NOT be saved.");
 }
 
 /**
