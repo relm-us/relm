c4535810d358b59dfe7bd958c9347a8e039d805c
WillQizza <williamqipizza@gmail.com>
2022-07-25 15:36:04 -0600

    tabbing will tab to the submit button

---
+++ client/src/ui/ButtonControls/ConnectionButton/components/ConnectionSubmitInput.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let label = "";
+</script>
+
+<r-connection-submit-input>
+  <input type="submit" value={label} />
+</r-connection-submit-input>
+
+<style>
+    input[type="submit"] {
+    color: #7f7f7f;
+    font-size: 4em;
+    background: none;
+    border: none;
+    outline: none;
+  }
+
+  input[type="submit"]:hover, input[type="submit"]:focus {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
