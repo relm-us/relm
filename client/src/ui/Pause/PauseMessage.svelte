296ff8147ccdb90aa3712b48d174802c9387b0f7
Duane Johnson <duane.johnson@gmail.com>
2022-08-24 14:08:17 -0600

    Improve Pause screen; fix z-index; fade in/out; fade world out

--- client/src/ui/Pause/PauseMessage.svelte
+++ client/src/ui/Pause/PauseMessage.svelte
@@ -1,51 +1,76 @@
 <script>
+  import { fade } from "svelte/transition";
+
   import Button from "~/ui/lib/Button";
   import ToggleSwitch from "~/ui/lib/ToggleSwitch";
   import { autoPause } from "~/stores/autoPause";
   import { worldManager } from "~/world";
 </script>
 
-<message>
-  <div class="paused">Paused</div>
-  <div class="button">
-    <Button on:click={() => worldManager.start()}>continue</Button>
-  </div>
-  <div class="toggle-auto-pause">
-    <ToggleSwitch
-      enabled={$autoPause}
-      labelOn="Autopause On"
-      labelOff="Autopause Off"
-      on:change={({ detail }) => ($autoPause = detail)}
-    />
-  </div>
-</message>
+<r-tint transition:fade>
+  <r-message>
+    <div class="paused">Paused</div>
+    <div class="button">
+      <Button on:click={() => worldManager.start()}>Continue</Button>
+    </div>
+    <div class="toggle-auto-pause">
+      <ToggleSwitch
+        enabled={$autoPause}
+        labelOn="Autopause On"
+        labelOff="Autopause Off"
+        on:change={({ detail }) => ($autoPause = detail)}
+      />
+    </div>
+  </r-message>
+</r-tint>
 
 <style>
-  message {
+  r-tint {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    z-index: 200;
+
+    background: rgba(0, 0, 0, 0.8);
+  }
+  r-message {
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    position: absolute;
-    z-index: 3;
-    top: 40%;
-    left: 50%;
-    transform: translate(-50%);
+    color: var(--background-gray, black);
 
-    font-size: 6vw;
-    color: white;
-
-    padding: 16px 32px 24px 32px;
-    background: var(--background-transparent-gray, rgba(40, 40, 40, 0.9));
+    padding: 16px 48px;
+    background: var(--foreground-white, white);
     border-radius: 5px;
   }
 
+  r-message > * {
+    margin: 16px 0;
+  }
+
+  .paused {
+    font-size: 3rem;
+    font-family: Garamond, "Times New Roman", Times, serif;
+    font-weight: bold;
+  }
+
   .button {
-    margin: 16px 0 12px 0;
+    --bg-color: var(--selected-red, red);
+    --fg-color: white;
+  }
+  .button :global(button) {
+    border: 1px solid #d67;
   }
 
   .toggle-auto-pause {
-    margin-top: 16px;
     font-size: 12px;
   }
 </style>
