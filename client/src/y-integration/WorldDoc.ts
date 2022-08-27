d9b1e567707018b7d196ae072b4e7928fd11e299
WillQizza <williamqipizza@gmail.com>
2022-08-26 22:51:30 -0600

    remove import

--- client/src/y-integration/WorldDoc.ts
+++ client/src/y-integration/WorldDoc.ts
@@ -1,4 +1,4 @@
-import { AuthenticationHeaders, yTextToDelta } from "relm-common";
+import type { AuthenticationHeaders } from "relm-common";
 import type { DecoratedECSWorld, WorldDocStatus } from "~/types";
 
 import * as Y from "yjs";
