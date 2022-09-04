d85855ce4e3e3073c3e8b86fee7ec49121673967
Duane Johnson <duane.johnson@gmail.com>
2022-09-04 09:11:10 -0600

    Fix escape key to unpause; add escape key to sign in/up dialogs

--- client/src/events/onEscape.ts
+++ client/src/events/onEscape.ts
@@ -1,18 +1,21 @@
 import { get } from "svelte/store";
 import { worldManager } from "~/world";
 
-import { selectedEntities } from "~/stores/selection";
-import { chatOpen } from "~/stores/chat";
 import { worldUIMode } from "~/stores/worldUIMode";
+import { chatOpen } from "~/stores/chat";
+import { openDialog } from "~/stores/openDialog";
+import { selectedEntities } from "~/stores/selection";
 
 import { onSwitchMode } from "./onSwitchMode";
 
 export function onEscape() {
   const selected = get(selectedEntities);
   if (!worldManager.started) {
-    worldManager.start();
+    worldManager.togglePaused();
   } else if (selected.size > 0) {
     worldManager.selection.clear();
+  } else if (get(openDialog) !== null) {
+    openDialog.set(null);
   } else if (get(chatOpen)) {
     chatOpen.set(false);
   } else if (get(worldUIMode) !== "play") {
