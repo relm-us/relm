cc9fc5e49b7ed91a8dd30ca15922039c21731cdf
Duane Johnson <duane.johnson@gmail.com>
2022-09-08 12:38:07 -0600

    Close color picker when clicking elsewhere

--- client/src/ui/AvatarBuilder/ColorPick.svelte
+++ client/src/ui/AvatarBuilder/ColorPick.svelte
@@ -1,24 +1,38 @@
 <script lang="ts">
   import ColorPicker from "~/ui/lib/ColorPicker";
+  import { hasAncestor } from "~/utils/hasAncestor";
+
   export let value;
 
+  let enabled = false;
+  let container;
+
+  function cancelPicker(event) {
+    if (!hasAncestor(event.target, container)) {
+      enabled = false;
+    }
+  }
 </script>
 
-<container>
-<ColorPicker
-  bind:value
-  enableSwatches={true}
-  enableAlpha={false}
-  open={false}
-  width="24px"
-  height="24px"
-  on:change
-/>
+<container bind:this={container}>
+  <ColorPicker
+    bind:value
+    enableSwatches={true}
+    enableAlpha={false}
+    bind:open={enabled}
+    width="24px"
+    height="24px"
+    on:change
+  />
 </container>
+
+<svelte:window on:mousedown={cancelPicker} />
 
 <style>
   container {
+    position: relative;
     display: block;
     margin-left: 10px;
+    z-index: 100;
   }
 </style>
