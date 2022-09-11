4fe83a15ef7e7bd2c07b11dd52dfa5ac0037996b
Duane Johnson <duane.johnson@gmail.com>
2022-09-11 14:54:30 -0600

    Change hair color options

--- common/src/identity/constants.ts
+++ common/src/identity/constants.ts
@@ -5,5 +5,5 @@ export const AVAILABLE_SKIN_COLORS = [
 
 // prettier-ignore
 export const AVAILABLE_HAIR_COLORS = [
-  "#debe99", "#aa8866", "#241c11", "#0a0a0a", "#9a3300"
+  "#debe99", "#af855f", "#3d2107", "#20150b", "#0a0a0a", "#9a3300", "#f1bbdf"
 ];
