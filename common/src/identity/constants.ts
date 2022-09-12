5ee992dc015ecd4be9e0c4467544695cc64c641f
Duane Johnson <duane.johnson@gmail.com>
2022-09-12 15:45:01 -0600

    Slight adjustment to skin and hair colors

--- common/src/identity/constants.ts
+++ common/src/identity/constants.ts
@@ -1,9 +1,9 @@
 // prettier-ignore
 export const AVAILABLE_SKIN_COLORS = [
-  "#d5b9ad", "#bbaa96", "#947159", "#a68865", "#855a32", "#57351d", "#2c1e0b"
+  "#d0b4a8", "#c2a482", "#b08759", "#947159", "#855a32", "#57351d", "#2c1e0b"
 ];
 
 // prettier-ignore
 export const AVAILABLE_HAIR_COLORS = [
-  "#debe99", "#af855f", "#3d2107", "#20150b", "#0a0a0a", "#9a3300", "#f1bbdf"
+  "#debe99", "#af855f", "#3d2107", "#20150b", "#0a0a0a", "#9a3300", "#f593df"
 ];
