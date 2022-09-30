bad20520849efc3cc59b56bf061e93e8d464f315
Duane Johnson <duane.johnson@gmail.com>
2022-09-29 20:32:59 -0600

    Possible fix for Relm slowdown due to physics engine overwork

--- client/src/ecs/plugins/physics/systems/createFixedTimestep.ts
+++ client/src/ecs/plugins/physics/systems/createFixedTimestep.ts
@@ -1,3 +1,5 @@
+const MAX_STEPS = 5;
+
 // Return a function that calls a callback as many times
 // as needed in order to "catch up" to the current time
 export function createFixedTimestep(
@@ -13,14 +15,21 @@ export function createFixedTimestep(
     } else {
       // catch up via multiple physics engine steps
       accumulator += delta;
+      let step = 0;
       while (accumulator >= timestep) {
         callback(timestep);
-        if (accumulator >= 1) {
+        if (step >= MAX_STEPS) {
+          // give up, we've reached max allowable physics steps
+          accumulator = 0;
+          break;
+        } else if (accumulator >= 1) {
           // give up, too slow to catch up
           accumulator = 0;
+          break;
         } else {
           accumulator -= timestep;
         }
+        step++;
       }
     }
   };
