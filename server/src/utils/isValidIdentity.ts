47b7e771f23179ed2e54e138b6997e00f14a594e
Duane Johnson <duane.johnson@gmail.com>
2022-10-27 13:01:31 -0600

    Don't check for valid color on login

--- server/src/utils/isValidIdentity.ts
+++ server/src/utils/isValidIdentity.ts
@@ -1,18 +1,19 @@
-import { isValidColor, isValidAppearance, PlayerStatus } from "relm-common";
+import { isValidAppearance, PlayerStatus } from "relm-common";
 
 function isValidStatus(status) {
   return (["initial", "present", "away"] as PlayerStatus[]).includes(status);
 }
 
 export function isValidIdentity(identityPayload) {
-  return (typeof identityPayload === "object")
-            && (typeof identityPayload.name === "string")
-            && (typeof identityPayload.color === "string" && isValidColor(identityPayload.color))
-            && (isValidStatus(identityPayload.status))
-            && (
-              ((typeof identityPayload.appearance === "object") && isValidAppearance(identityPayload.appearance))
-                || (typeof identityPayload.appearance === "undefined")
-            )
-            && (typeof identityPayload.equipment === "object" || typeof identityPayload.equipment === "undefined");
-
+  return (
+    typeof identityPayload === "object" &&
+    typeof identityPayload.name === "string" &&
+    typeof identityPayload.color === "string" &&
+    isValidStatus(identityPayload.status) &&
+    ((typeof identityPayload.appearance === "object" &&
+      isValidAppearance(identityPayload.appearance)) ||
+      typeof identityPayload.appearance === "undefined") &&
+    (typeof identityPayload.equipment === "object" ||
+      typeof identityPayload.equipment === "undefined")
+  );
 }
