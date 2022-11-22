3d553bd195bff02617b002351f1b792e164259bf
Duane Johnson <duane.johnson@gmail.com>
2022-11-22 11:57:25 -0700

    Simplify AnimationSystem and make typesafe

--- client/src/ecs/plugins/animation/components/MixerRef.ts
+++ client/src/ecs/plugins/animation/components/MixerRef.ts
@@ -1,6 +1,11 @@
+import { AnimationAction, AnimationMixer } from "three";
 import { LocalComponent, RefType } from "~/ecs/base";
 
 export class MixerRef extends LocalComponent {
+  value: AnimationMixer;
+  activeAction: AnimationAction;
+  previousAction: AnimationAction;
+
   static props = {
     value: {
       type: RefType,
--- client/src/ecs/plugins/animation/systems/AnimationSystem.ts
+++ client/src/ecs/plugins/animation/systems/AnimationSystem.ts
@@ -48,45 +48,30 @@ export class AnimationSystem extends System {
 
   setAnimation(entity: Entity) {
     const spec = entity.get(Animation);
-    const mixer = entity.get(MixerRef)?.value;
+    const mixer: MixerRef = entity.get(MixerRef);
     if (!mixer) return;
 
     const ref: ModelRef = entity.get(ModelRef);
 
-    if (spec.transition === 0) {
-      mixer.stopAllAction();
-      mixer.uncacheRoot(mixer.getRoot());
+    mixer.previousAction = mixer.activeAction;
+    const clip = ref.value.animations.find((c) => c.name === spec.clipName);
+    if (!clip) return;
 
-      mixer.timeScale = spec.timeScale;
-      const clip = ref.value.animations.find((c) => c.name === spec.clipName);
-      if (clip) {
-        mixer.activeAction = mixer.clipAction(clip).reset().play();
-        if (!spec.loop) {
-          mixer.activeAction.setLoop(LoopOnce);
-          mixer.activeAction.clampWhenFinished = true;
-        }
-      }
-    } else {
-      mixer.previousAction = mixer.activeAction;
-      const clip = ref.value.animations.find((c) => c.name === spec.clipName);
-      if (!clip) return;
+    mixer.activeAction = mixer.value.clipAction(clip);
 
-      mixer.activeAction = mixer.clipAction(clip);
+    if (mixer.previousAction && mixer.previousAction !== mixer.activeAction) {
+      mixer.previousAction.fadeOut(spec.transition);
+    }
 
-      if (mixer.previousAction && mixer.previousAction !== mixer.activeAction) {
-        mixer.previousAction.fadeOut(spec.transition);
-      }
-
-      mixer.activeAction
-        .reset()
-        .setEffectiveTimeScale(1)
-        .setEffectiveWeight(1)
-        .fadeIn(spec.transition)
-        .play();
-      if (!spec.loop) {
-        mixer.activeAction.setLoop(LoopOnce);
-        mixer.activeAction.clampWhenFinished = true;
-      }
+    mixer.activeAction
+      .reset()
+      .setEffectiveTimeScale(spec.timeScale)
+      .setEffectiveWeight(1)
+      .fadeIn(spec.transition)
+      .play();
+    if (!spec.loop) {
+      mixer.activeAction.setLoop(LoopOnce, 1);
+      mixer.activeAction.clampWhenFinished = true;
     }
   }
 
