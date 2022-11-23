416dd52ea893188ac394e9356951d29d4852a455
Duane Johnson <duane.johnson@gmail.com>
2022-11-23 08:09:10 -0700

    Use i18n strings for NeedsMigration dialog

--- client/src/ui/Dialogs/NeedsMigrationDialog.svelte
+++ client/src/ui/Dialogs/NeedsMigrationDialog.svelte
@@ -7,6 +7,7 @@
   import { worldManager } from "~/world";
 
   let upgrading = false;
+  let upgraded = false;
 
   function upgradeWorld() {
     if (upgrading) return;
@@ -15,7 +16,7 @@
 
     worldManager.upgradeWorld();
 
-    setTimeout(() => dispatch("cancel"), 1000);
+    setTimeout(() => (upgraded = true), 2000);
   }
 
   const dispatch = createEventDispatcher();
@@ -23,25 +24,26 @@
 
 <Dialog title={$_("NeedsMigrationDialog.title")} on:cancel>
   <r-container>
-    <p>
-      Relm's software has changed since last this world was edited. Please
-      upgrade this world.
-    </p>
-    <p>
-      You can skip this step, but you might encounter some issues with build
-      mode or play mode interactions.
-    </p>
-    <div class="buttons">
-      <Button on:click={upgradeWorld}>
-        {$_("NeedsMigrationDialog.upgrade")}
-      </Button>
-      <Button
-        on:click={() => dispatch("cancel")}
-        style="--bg-color: var(--background-gray); --bg-hover-color: #21232A; --fg-color: var(--foreground-gray)"
-      >
-        {$_("NeedsMigrationDialog.skip")}
-      </Button>
-    </div>
+    {#if !upgraded}
+      <p>{$_("NeedsMigrationDialog.software_changed")}</p>
+      <p>{$_("NeedsMigrationDialog.explain_skip")}</p>
+      {#if upgrading}
+        <p>{$_("NeedsMigrationDialog.upgrading")}</p>
+      {/if}
+      <div class="buttons">
+        <Button on:click={upgradeWorld}>
+          {$_("NeedsMigrationDialog.upgrade")}
+        </Button>
+        <Button
+          on:click={() => dispatch("cancel")}
+          style="--bg-color: var(--background-gray); --bg-hover-color: #21232A; --fg-color: var(--foreground-gray)"
+        >
+          {$_("NeedsMigrationDialog.skip")}
+        </Button>
+      </div>
+    {:else}
+      <p>{$_("NeedsMigrationDialog.done_refresh")}</p>
+    {/if}
   </r-container>
 </Dialog>
 
